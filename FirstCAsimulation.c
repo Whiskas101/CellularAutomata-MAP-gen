659924b5b878ec628d8316ef699af66c548c70eb
Whiskas101 <getintouchwithvic@gmail.com>
2023-05-09 19:06:09 +0530

    incomplete (Bin to Dec)
    work on the binary to dec converter

--- FirstCAsimulation.c
+++ FirstCAsimulation.c
@@ -1,1 +1,48 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+// Cellular automaton 1-D visualisation
+// only using arrays
+
+
+//lets assume a 51x51 grid
+#define size 51
+int row[size];
+
+
+int binaryToDecimal(int c, int b, int a){
+
+    if((c==0 || c==1 )&& c==b && c == a) return 0;
+    
+
+}
+
+
+
+int main(){
+
+    //first we initialise it to zero for all values
+
+    for(int i = 0; i < size ; i++){
+
+        row[i] = 0;
+
+    }
+    
+
+
+
+
+
+
+    printf("yes");
+    int temp = 0;
+    scanf("%d", &temp);
+    return 0;
+}
+
+
+
+
+
+
